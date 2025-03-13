bd09cc60ff050868367b7b740dc5fd88dbaf085c
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-14 02:43:48 +0800

    Program 4 Batch 2

---
+++ Batch_2/Program_4_Batch_2.py
@@ -0,0 +1,6 @@
+#Program 4: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+quotient = num1 // num2
+print(quotient)
