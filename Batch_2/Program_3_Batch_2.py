cd34c12ba90b33be245ac66afeb04159656d7f76
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-14 02:40:44 +0800

    Program 3 Batch 2

---
+++ Batch_2/Program_3_Batch_2.py
@@ -0,0 +1,6 @@
+#Program 3: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+difference = num1 - num2
+print(difference)
