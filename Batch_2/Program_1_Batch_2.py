58048696ff8defb8c80c622fc2cd0fa4f3c613cd
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-14 02:33:22 +0800

    Program 1 Batch 2

---
+++ Batch_2/Program_1_Batch_2.py
@@ -0,0 +1,8 @@
+#Program 1: Create a program that ask user to input 2 numbers. Print the smaller number.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 < num2:
+    print(num1)
+else:
+    print(num2)
