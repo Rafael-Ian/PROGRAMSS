b80e98fe1fe33a3e6c4d83d01bb9c36cb0868d99
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-14 00:07:21 +0800

    Prog 3 Batch 1

---
+++ Program_3_Batch_1.py
@@ -0,0 +1,6 @@
+#Program 3: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+print("Sum: ", sum)
