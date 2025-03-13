db404c74e91d5e3ad43085e58703f04a7047501f
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-14 00:15:02 +0800

    Program 5 Batch 1

---
+++ Program_5_Batch_1.py
@@ -0,0 +1,6 @@
+#Program 5: Create a program that ask user to input two numbers. Print the quotient of the two number with the decimal point.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+quotient = num1 / num2
+print("Quotient: ", quotient)
