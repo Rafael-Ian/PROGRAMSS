cac21f318629ba123b1e4daecf0c8582161009e4
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-13 23:59:50 +0800

    Prog 2 Batch 1

---
+++ Program 2 Batch 1.py
@@ -0,0 +1,9 @@
+#Program 2: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 == num2:
+    print("Equal")
+else:
+    print("Not equal")
+
