467382af1b3dab9cc5537beff0ee335ac9953d6b
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-14 00:12:01 +0800

    Program 4 Batch 1

---
+++ Program_4_Batch_1.py
@@ -0,0 +1,6 @@
+#Program 4: Create a program that ask user to input two numbers. Print the product of the two numbers.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+product = num1 * num2
+print("Product: ", product)
