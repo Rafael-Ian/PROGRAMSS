b00abef436e60cf32a1ac61945ec88028d0cfd38
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-14 18:35:01 +0800

    Program 5 Batch 4

--- Batch_4/displays_the_average.py
+++ Batch_4/displays_the_average.py
@@ -1,1 +1,10 @@
 #Program 5: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+numbers = []
+
+while True:
+    try:
+        numbers.append(float(input("Enter number: ")))
+    except:
+        break
+#Calculates and displays the average
+print("Average: ", sum(numbers) / len(numbers) if numbers else "No numbers were entered.")
